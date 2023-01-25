310f39067d53ed363f09d5d681ea15dfe9c299c2
William Blanchet Lafrenière <willclash@outlook.com>
2023-01-25 16:52:13 -0500

    merge

--- src/src/engine/components/ComponentEmployeeList.tsx
+++ src/src/engine/components/ComponentEmployeeList.tsx
@@ -10,12 +10,35 @@ export class ComponentEmployeeList extends React.Component {
 
   public render(): JSX.Element {
     return (
-      <div className="container">
-        <div>
-          {this.list.map((elem) => (
-            <div>{elem}</div>
-          ))}
-        </div>
+      <div className="mt-5">
+        <h3>Liste des employés</h3>
+        <Table responsive striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Prénom</th>
+              <th>Nom</th>
+              <th>Tel</th>
+              <th>Gestionnaire</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.from({ length: this.list.length }).map((_, index) => (
+              <tr>
+                <td key={index}>{index}</td>
+                <td key={index + "a"}>Nom</td>
+                <td key={index + "b"}>
+                  <a>{this.list[index]}</a>
+                </td>
+                <td key={index + "c"}>Tel</td>
+                <td key={index + "d"}>Gest</td>
+                <td key={index + "e"}>Role</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <Button href="/creer-employe">Ajouter</Button>
       </div>
     );
   }
--- src/src/engine/types/EmployeeList.ts
+++ src/src/engine/types/EmployeeList.ts
@@ -1,6 +1,17 @@
 export interface EmployeeList {
     list: string[];
 }
+
+export interface Employee {
+    name: string,
+    firstName: string,
+    phoneNumber: string,
+    active: boolean,
+    jobTitle: string,
+    skills: string
+}
+
+
 export interface EmployeeObject {
     [key: string] : EmployeeList;
 }
