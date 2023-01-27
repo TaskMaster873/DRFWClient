48c5c041e5bc1c165f23157d2413d842c61e8675
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2023-01-26 19:10:57 -0500

    Update index.tsx

--- src/src/engine/pages/index.tsx
+++ src/src/engine/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { Logger } from "../Logger";
-import {NavigationBar} from "../components/NavigationBar";
 
 export class Index extends React.Component {
   private logger: Logger = new Logger(`Index`, `#20f6a4`, false);
