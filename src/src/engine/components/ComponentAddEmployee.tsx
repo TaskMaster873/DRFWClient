fa28ff03b0cf80231df439b8f270d0e0644dcb9c
DJLawton <70708792+DJLawton@users.noreply.github.com>
2023-01-30 13:26:37 -0500

    Validation working, no tests required

--- src/src/engine/components/ComponentAddEmployee.tsx
+++ src/src/engine/components/ComponentAddEmployee.tsx
@@ -3,9 +3,10 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { LinkContainer } from "react-router-bootstrap";
 
 /**
- * 
+ *
  * Ceci est le composant pour ajouter les employés
  */
 export class ComponentAddEmployee extends React.Component<
@@ -38,25 +39,25 @@ export class ComponentAddEmployee extends React.Component<
             <Form.Control
               required
               type="text"
-              placeholder="000000"
+              placeholder="NE: 000000"
               pattern="^[1-9]{6}$"
             />
             <Form.Control.Feedback type="invalid">
-              Un numéro d'employé contient 6 chiffres
+              Un numéro d'employé est nécessaire
             </Form.Control.Feedback>
           </Form.Group>
           <Form.Group as={Col} md="4" controlId="formPrenom">
             <Form.Label>Prénom</Form.Label>
             <Form.Control required type="text" placeholder="Prénom" />
             <Form.Control.Feedback type="invalid">
-              L'employé a besoin d'un prénom
+              Un prénom est nécessaire
             </Form.Control.Feedback>
           </Form.Group>
           <Form.Group as={Col} md="4" controlId="formNom">
             <Form.Label>Nom</Form.Label>
             <Form.Control required type="text" placeholder="Nom" />
             <Form.Control.Feedback type="invalid">
-              L'employé a besoin d'un nom
+              Un nom est nécessaire
             </Form.Control.Feedback>
           </Form.Group>
         </Row>
@@ -67,17 +68,17 @@ export class ComponentAddEmployee extends React.Component<
               required
               type="tel"
               pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-              placeholder="000-000-0000"
+              placeholder="Tel: 000-000-0000"
             />
             <Form.Control.Feedback type="invalid">
-              Un numéro de téléphone conteitn trois séries de chiffres séparé par des tirets
+              Un numéro de téléphone est nécessaire
             </Form.Control.Feedback>
           </Form.Group>
           <Form.Group as={Col} md="6" controlId="formPassword">
             <Form.Label>Mot de passe initial</Form.Label>
             <Form.Control required type="password" placeholder="Mot de passe" />
             <Form.Control.Feedback type="invalid">
-              L'employé a besoin d'un mot de passe initial
+              Un mot de passe initial est nécessaire
             </Form.Control.Feedback>
           </Form.Group>
         </Row>
@@ -93,19 +94,21 @@ export class ComponentAddEmployee extends React.Component<
               />
             ))}
           </Form.Group>
-        <Form.Group as={Col} md="6" controlId="formRole">
-          <Form.Label>Rôle de l'employé</Form.Label>
-          <Form.Select required aria-label="Default select example">
-            {this.roles.map((role) => (
-              <option key={`${role}`} value={`${role}`}>{`${role}`}</option>
-            ))}
-          </Form.Select>
-        </Form.Group>
+          <Form.Group as={Col} md="6" controlId="formRole">
+            <Form.Label>Rôle de l'employé</Form.Label>
+            <Form.Select required aria-label="Default select example">
+              {this.roles.map((role) => (
+                <option key={`${role}`} value={`${role}`}>{`${role}`}</option>
+              ))}
+            </Form.Select>
+          </Form.Group>
         </Row>
         <div className="d-flex justify-content-center">
-          <Button className="mb-3 me-4 btn-lg" variant="secondary">
-            Retour
-          </Button>
+          <LinkContainer to="/employees">
+            <Button className="mb-3 me-4 btn-lg" variant="secondary">
+              Retour
+            </Button>
+          </LinkContainer>
           <Button className="mb-3 btn-lg" variant="primary" type="submit">
             Créer
           </Button>
