2000b6cafe3533233b893b8911c40681a0ce98ea
RominMartignat <rominmart810@gmail.com>
2023-01-31 12:58:39 -0500

    commit regardes ça

--- src/src/engine/components/ComponentSchedule.tsx
+++ src/src/engine/components/ComponentSchedule.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { EmployeeList, Employee } from "../types/Employee";
 import { DayPilot, DayPilotCalendar } from "@daypilot/daypilot-lite-react";
+import { ResourceGroups } from "./ComponentFilterProjects";
 
 /**
  * Ceci est le composant d'horaire
@@ -9,9 +10,11 @@ export class ComponentSchedule extends React.Component {
 
     private list: Employee[] = [];
     private datePicker: DayPilot.DatePicker;
-    private dateRef /*= React.createRef()*/;
+    private dateRef: React.RefObject<unknown> /*= React.createRef()*/;
+    private calendarRef;
     constructor(props: EmployeeList) {
         super(props);
+        this.calendarRef = React.createRef();
         this.list = props.list;
         this.dateRef = React.createRef(); //DayPilot.Date.today();
         this.state = {
@@ -34,7 +37,21 @@ export class ComponentSchedule extends React.Component {
                 { name: "Room 7", id: "R7" },
             ],*/
             events: this.doEvents(),
+            onHeaderClick: async (args: { column: { name: any; data: { name: any; }; }; }) => {
+                const modal = await DayPilot.Modal.prompt("Resource name:", args.column.name);
+                if (!modal.result) { return; }
+                args.column.data.name = modal.result;
+                this.calendar.update();
+            },
         };
+    }
+
+    get calendar() {
+       return this.calendarRef.current.control;
+    }
+
+    private loadCalendarData() {
+
     }
 
     /**
@@ -76,6 +93,7 @@ export class ComponentSchedule extends React.Component {
 
     }
 
+
     private getDateRef() {
         return this.dateRef;
     }
@@ -84,36 +102,86 @@ export class ComponentSchedule extends React.Component {
         this.datePicker.show();
     }
 
+    loadGroups() {
+        const data = [
+            {
+                name: "Locations", id: "locations", resources: [
+                    { name: "Room 1", id: "R1" },
+                    { name: "Room 2", id: "R2" },
+                    { name: "Room 3", id: "R3" },
+                    { name: "Room 4", id: "R4" },
+                    { name: "Room 5", id: "R5" },
+                    { name: "Room 6", id: "R6" },
+                    { name: "Room 7", id: "R7" },
+                ]
+            },
+            {
+                name: "People", id: "people", resources: [
+                    { name: "Person 1", id: "P1" },
+                    { name: "Person 2", id: "P2" },
+                    { name: "Person 3", id: "P3" },
+                    { name: "Person 4", id: "P4" },
+                    { name: "Person 5", id: "P5" },
+                    { name: "Person 6", id: "P6" },
+                    { name: "Person 7", id: "P7" },
+                ]
+            },
+            {
+                name: "Tools", id: "tools", resources: [
+                    { name: "Tool 1", id: "T1" },
+                    { name: "Tool 2", id: "T2" },
+                    { name: "Tool 3", id: "T3" },
+                    { name: "Tool 4", id: "T4" },
+                    { name: "Tool 5", id: "T5" },
+                    { name: "Tool 6", id: "T6" },
+                    { name: "Tool 7", id: "T7" },
+                ]
+            },
+        ];
+        return data;
+    }
+
+    groupChanged(group: { resources: any; }) {
+
+        const columns = group.resources;
+
+        const events = [
+            {
+                id: 1,
+                text: "Event 1",
+                start: "2022-11-07T10:30:00",
+                end: "2022-11-07T13:00:00",
+                barColor: "#fcb711",
+                resource: "R1"
+            },
+            // ...
+        ];
+        this.setState({columns: columns, events: events});
+        this.calendar.update({ columns, events });
+
+    }
+
+
     public render(): JSX.Element {
-        
+
         if (this.list === undefined || this.list.length == 0) {
             return (
                 <div> Il n'y a pas d'horaire à voir</div>
             );
         }
         else return (
-           // <div>
+            // <div>
             //    <div className="toolbar">
-             //       <span ref={this.dateRef}></span> <button onClick={ev => this.changeDate()}>Change date</button>
-             //   </div>
-                <div>
-                    <DayPilotCalendar
-                        {...this.state} />
-                </div>
+            //       <span ref={this.dateRef}></span> <button onClick={ev => this.changeDate()}>Change date</button>
+            //   </div>
+            <div>
+                <ResourceGroups onChange={args => this.groupChanged(args.selected)} items={this.loadGroups()}></ResourceGroups>
+                <DayPilotCalendar
+                    {...this.state}
+                    ref={this.calendarRef} />
+            </div>
 
-           // </div>
-
-            /* <DayPilotCalendar businessBeginsHour={8} 
-                    businessEndsHour={20}
-                    heightSpec="Full"
-                    height={2000}
-                    cellHeight={20}
-                    cellDuration={5}
-                    viewType={"Resources"}
-                    columns={this.doColumns()}
-                    events={this.doEvents()}
-                    showNonBusiness={false}
-                ></DayPilotCalendar>*/
+            // </div>
         );
     }
 }
