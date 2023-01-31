cf4ecaf1f5b60b65bfae7dd5d8099cae835f9396
RominMartignat <rominmart810@gmail.com>
2023-01-31 16:36:36 -0500

    Revert "commit footerchange"
    This reverts commit 554bc74622e572e49402e71e32ccbbd8f3433913.

--- src/src/engine/components/FooterBar.tsx
+++ src/src/engine/components/FooterBar.tsx
@@ -11,40 +11,30 @@ import Logo from "../../deps/images/logo.png";
 
 export class FooterBar extends React.Component {
   private logger: Logger = new Logger(`NavigationBar`, `#20f6a4`);
-  private isLoggedIn: boolean = false;
+
   public render(): JSX.Element {
     return (
       <div>
-        <div className="pt-5 pb-4 "></div>
+        <div className="pt-5 pb-4"></div>
         <Navbar variant="dark" fixed="bottom">
-          <Container  className="d-flex flex-wrap justify-content-between align-items-center">
-          <LinkContainer to="/">
+          <Container className="d-flex flex-wrap justify-content-between align-items-center">
+            <Nav className="ms-2">
+              <Navbar.Text>Équipe DRFW</Navbar.Text>
+            </Nav>
+            <LinkContainer to="/">
               <Navbar.Brand>
                 <img
-                  className="ms-2"
+                  className="ms-4"
                   src={Logo}
                   alt="Logo TaskMaster"
-                  width={25}
-                  height={30}
+                  width={50}
+                  height={60}
                 />
               </Navbar.Brand>
             </LinkContainer>
-            <Nav className="px-2 fs-6">
-              <Navbar.Text>Équipe DRFW</Navbar.Text>
-            </Nav>
-            <Nav className="px-2 fs-6 ">
+            <Nav className="me-2">
               <LinkContainer to="/login">
-                <Nav.Link>{this.loginButton()}</Nav.Link>
-              </LinkContainer>
-            </Nav>
-            <Nav className="px-2 fs-6">
-              <LinkContainer to="/schedule">
-                <Nav.Link>Horaire</Nav.Link>
-              </LinkContainer>
-            </Nav>
-            <Nav className="px-2 fs-6">
-              <LinkContainer to="/about">
-                <Nav.Link>À propos</Nav.Link>
+                <Nav.Link>Connexion</Nav.Link>
               </LinkContainer>
             </Nav>
           </Container>
@@ -52,29 +42,4 @@ export class FooterBar extends React.Component {
       </div>
     );
   }
-
-  /**
-   *
-   * @returns se déconnecter si la personne est connectée
-  * */
-
-  private loginButton() {
-    if (this.isLoggedIn) {
-      return <Nav.Link onClick={this.logOut}>Se déconnecter</Nav.Link>;
-    } else {
-      return (
-        <LinkContainer to="/login">
-          <Nav.Link id="loginLink">Connexion</Nav.Link>
-        </LinkContainer>
-      );
-    }
-  }
-
-  /**
-   * Je ne sais pas si il faudrait le garder
-   */
-  private logOut() {
-    this.logger.log(`Logging out`);
-  }
 }
-
