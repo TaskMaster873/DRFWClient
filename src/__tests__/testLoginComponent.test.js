4b7564e45299f07f25183d64c92e74395c279654
William Blanchet Lafrenière <willclash@outlook.com>
2023-02-02 15:58:37 -0500

    fix tests

--- src/__tests__/testLoginComponent.test.js
+++ src/__tests__/testLoginComponent.test.js
@@ -1,7 +1,7 @@
 import "@testing-library/react/dist/fire-event";
 import "@testing-library/jest-dom";
 import {fireEvent, render} from "@testing-library/react";
-import { FormErrorType } from "../src/engine/errors/FormErrorType";
+import {FormErrorType} from "../src/engine/errors/FormErrorType";
 import testConstants from "../Constants/testConstants";
 import userEvent from "@testing-library/user-event";
 import {MemoryRouter} from "react-router-dom";
@@ -11,66 +11,67 @@ import {Config} from "../src/engine/config/Config";
 
 let user;
 beforeEach(async () => {
-  user = userEvent.setup();
-  render(<MemoryRouter><Login /></MemoryRouter>);
+    user = userEvent.setup();
+    render(<MemoryRouter><Login/></MemoryRouter>);
 });
 
 test("should render form inputs", async () => {
-  const { inputPassword, form, inputNo } = getFields();
+    const {inputPassword, form, inputNo} = getFields();
 
-  expect(form).not.toBeNull();
-  expect(inputNo).not.toBeNull();
-  expect(inputPassword).not.toBeNull();
-  expect(inputNo).toHaveAttribute("type", "number");
-  expect(inputPassword).toHaveAttribute("type", "password");
+    expect(form).not.toBeNull();
+    expect(inputNo).not.toBeNull();
+    expect(inputPassword).not.toBeNull();
+    expect(inputNo).toHaveAttribute("type", "number");
+    expect(inputPassword).toHaveAttribute("type", "password");
 });
 
 describe("Empty Fields login validation", () => {
-  test("Empty employee number should show error", async () => {
-    const { inputPassword, form, inputNo } = getFields();
+    test("Empty employee number should show error", async () => {
+        const {inputPassword, form, inputNo} = getFields();
 
+        await user.type(inputPassword, testConstants.validPassword);
+
+        fireEvent.submit(form);
+
+        expect(inputNo.value).toBe("");
+        expect(inputPassword.value).toBe(testConstants.validPassword);
+        expect(form.classList.contains("was-validated")).toBeTruthy();
+        expect(form.dataset.error).toBe(FormErrorType.INVALID_FORM);
+    });
+
+    test("Empty password should show error", async () => {
+        const {inputPassword, form, inputNo} = getFields();
+
+        await user.type(inputNo, testConstants.validNoEmployee);
+
+        fireEvent.submit(form);
+
+        expect(inputNo.value).toBe(testConstants.validNoEmployee);
+        expect(inputPassword.value).toBe("");
+        expect(form.classList.contains("was-validated")).toBeTruthy();
+        expect(form.dataset.error).toBe(FormErrorType.INVALID_FORM);
+    });
+});
+
+test("Valid employee number and password should submit form", async () => {
+    Config.loginWithPassword = jest.fn();
+    const {inputPassword, form, inputNo} = getFields();
+
+    await user.type(inputNo, testConstants.validNoEmployee);
     await user.type(inputPassword, testConstants.validPassword);
 
     fireEvent.submit(form);
 
-    expect(inputNo.value).toBe("");
+    expect(Config.loginWithPassword).toBeCalled();
+    expect(inputNo.value).toBe(testConstants.validNoEmployee);
     expect(inputPassword.value).toBe(testConstants.validPassword);
     expect(form.classList.contains("was-validated")).toBeTruthy();
-    expect(form.dataset.error).toBe(FormErrorType.INVALID_FORM);
-  });
-
-  test("Empty password should show error", async () => {
-    const { inputPassword, form, inputNo } = getFields();
-
-    await user.type(inputNo, testConstants.validNoEmployee);
-
-    fireEvent.submit(form);
-
-    expect(inputNo.value).toBe(testConstants.validNoEmployee);
-    expect(inputPassword.value).toBe("");
-    expect(form.classList.contains("was-validated")).toBeTruthy();
-    expect(form.dataset.error).toBe(FormErrorType.INVALID_FORM);
-  });
+    expect(form.dataset.error).toBe(FormErrorType.NO_ERROR);
 });
 
-test("Valid employee number and password should submit form", async () => {
-  Config.loginWithPassword = jest.fn();
-  const { inputPassword, form, inputNo } = getFields();
-
-  await user.type(inputNo, testConstants.validNoEmployee);
-  await user.type(inputPassword, testConstants.validPassword);
-
-  fireEvent.submit(form);
-
-  expect(Config.loginWithPassword).toBeCalled();
-  expect(inputNo.value).toBe(testConstants.validNoEmployee);
-  expect(inputPassword.value).toBe(testConstants.validPassword);
-  expect(form.classList.contains("was-validated")).toBeTruthy();
-  expect(form.dataset.error).toBe(FormErrorType.NO_ERROR);
-});
 function getFields() {
-  const form = document.querySelector("form");
-  const inputNo = document.getElementById("noLogin");
-  const inputPassword = document.getElementById("passwordLogin");
-  return { inputPassword, form, inputNo };
+    const form = document.querySelector("form");
+    const inputNo = document.getElementById("noLogin");
+    const inputPassword = document.getElementById("passwordLogin");
+    return {inputPassword, form, inputNo};
 }
