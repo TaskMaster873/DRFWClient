9102896907b6433d380553f9aa89e61207295e94
RominMartignat <rominmart810@gmail.com>
2023-02-08 16:47:23 -0500

    ajout de 4 tests

--- src/__tests__/testComponentPopupSchedule.test.js
+++ src/__tests__/testComponentPopupSchedule.test.js
@@ -8,48 +8,91 @@ import { fireEvent, render } from "@testing-library/react";
 import { FormErrorType } from "../src/engine/messages/FormMessages";
 import testConstants from "../Constants/testConstants";
 import userEvent from "@testing-library/user-event";
-
+import {MemoryRouter} from "react-router-dom";
 
 let user;
 let matchMedia;
 
-describe('Test TaskMaster Client component', () => {
-    beforeAll(() => {
-        matchMedia = new MatchMediaMock();
-    });
+describe("Test TaskMaster Client component", () => {
+  beforeAll(() => {
+    matchMedia = new MatchMediaMock();
+  });
 
-    afterEach(() => {
-        matchMedia.clear();
-    });
-    test('jsdom is Initialized', () => {
-        const element = document.createElement('div');
-        expect(element).not.toBeNull();
-    });
+  afterEach(() => {
+    matchMedia.clear();
+  });
+  test("jsdom is Initialized", () => {
+    const element = document.createElement("div");
+    expect(element).not.toBeNull();
+  });
 
-    test('Test if match media is defined', () => {
-        const mediaQuery = '(prefers-color-scheme: light)';
-        const firstListener = jest.fn();
-        const secondListener = jest.fn();
-        const mql = window.matchMedia(mediaQuery);
+  test("Test if match media is defined", () => {
+    const mediaQuery = "(prefers-color-scheme: light)";
+    const firstListener = jest.fn();
+    const secondListener = jest.fn();
+    const mql = window.matchMedia(mediaQuery);
 
-        mql.addListener(ev => ev.matches && firstListener());
-        mql.addListener(ev => ev.matches && secondListener());
+    mql.addListener((ev) => ev.matches && firstListener());
+    mql.addListener((ev) => ev.matches && secondListener());
 
-        matchMedia.useMediaQuery(mediaQuery);
+    matchMedia.useMediaQuery(mediaQuery);
 
-        expect(firstListener).toBeCalledTimes(1);
-        expect(secondListener).toBeCalledTimes(1);
-    });
+    expect(firstListener).toBeCalledTimes(1);
+    expect(secondListener).toBeCalledTimes(1);
+  });
 
-    test('Render app without crashing', async () => {
-        const {Application} = require('../src/Application')
+  test("Render app without crashing", async () => {
+    const { Application } = require("../src/Application");
 
-        let app = new Application();
-        app.start();
-    });
+    let app = new Application();
+    app.start();
+  });
 
-    test('test dfsojsd', async () => {
-        const {ComponentPopupSchedule} = require("../src/engine/components/ComponentPopupSchedule");
-        expect(true);
-    })
+  test("test all are not rendered when isShowing is false", async () => {
+    const {ComponentPopupSchedule} = require("../src/engine/components/ComponentPopupSchedule");
+    user = userEvent.setup();
+    render(<ComponentPopupSchedule isShowing={false}/>);
+
+    const { form, inputName, inputColor } = getFields();
+
+    expect(form).toBeNull();
+    
+    expect(inputName).toBeNull();
+    expect(inputColor).toBeNull();
+  });
+  test("test all are rendered", async () => {
+    const {ComponentPopupSchedule} = require("../src/engine/components/ComponentPopupSchedule");
+    user = userEvent.setup();
+    render(<ComponentPopupSchedule isShowing={true}/>);
+
+    const { form, inputName, inputColor } = getFields();
+
+    expect(form).not.toBeNull();
+    
+    expect(inputName).not.toBeNull();
+    expect(inputColor).not.toBeNull();
+  });
+
+  test("test user add a name in the input for the name, should change de value of the input", async () => {
+    const {ComponentPopupSchedule} = require("../src/engine/components/ComponentPopupSchedule");
+    user = userEvent.setup();
+    render(<ComponentPopupSchedule isShowing={true}/>);
+
+    const { form, inputName, inputColor } = getFields();
+
+    await user.type(inputName, testConstants.validName);
+
+    expect(inputName.value).toBe(testConstants.validName);
+  });
 });
+
+function getFields() {
+  const form = document.querySelector("form");
+  const inputName = document.getElementById("nameOfEvent");
+  const inputColor = document.getElementById("colorOfEvent");
+  return {
+    form,
+    inputName,
+    inputColor,
+  };
+}
