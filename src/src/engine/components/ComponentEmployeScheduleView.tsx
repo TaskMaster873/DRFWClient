c0d7f71497956c6a2a2c081dc9861a6d646b7da3
RominMartignat <rominmart810@gmail.com>
2023-02-10 11:43:20 -0500

    commit

--- src/src/engine/components/ComponentEmployeScheduleView.tsx
+++ src/src/engine/components/ComponentEmployeScheduleView.tsx
@@ -1,119 +1,131 @@
 import React from 'react';
-import {DayPilot, DayPilotCalendar, DayPilotNavigator} from "@daypilot/daypilot-lite-react";
+import { DayPilot, DayPilotCalendar, DayPilotNavigator } from "@daypilot/daypilot-lite-react";
 
 const styles = {
-  wrap: {
-    display: "flex"
-  },
-  left: {
-    marginRight: "10px"
-  },
-  main: {
-    flexGrow: "1"
-  }
+    wrap: {
+        display: "flex"
+    },
+    left: {
+        marginRight: "10px"
+    },
+    main: {
+        flexGrow: "1"
+    },
+    oneHundred: {
+        height: "100vh"
+    }
 };
 
 export class ComponentEmployeScheduleView extends React.Component {
     calendarRef: React.RefObject<any>;
     datePickerRef: React.RefObject<any>;
-  constructor(props) {
-    super(props);
-    this.datePickerRef = React.createRef();
-    this.calendarRef = React.createRef();
-    this.state = {
-      viewType: "Week",
-      durationBarVisible: false,
-      timeRangeSelectedHandling: "Enabled",
-      onTimeRangeSelected: async args => {
-        const dp = this.calendar;
-        const modal = await DayPilot.Modal.prompt("Create a new event:", "Event 1");
-        dp.clearSelection();
-        if (!modal.result) { return; }
-        dp.events.add({
-          start: args.start,
-          end: args.end,
-          id: DayPilot.guid(),
-          text: modal.result
-        });
-      },
-      eventDeleteHandling: "Update",
-      onEventClick: async args => {
-        const dp = this.calendar;
-        const modal = await DayPilot.Modal.prompt("Update event text:", args.e.text());
-        if (!modal.result) { return; }
-        const e = args.e;
-        e.data.text = modal.result;
-        dp.events.update(e);
-      },
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.datePickerRef = React.createRef();
+        this.calendarRef = React.createRef();
+        this.state = {
+            viewType: "Week",
+            durationBarVisible: false,
+            timeRangeSelectedHandling: "Enabled",
+            eventResizeHandling: "Disabled",
+            eventMoveHandling: "Disabled",
+            eventDeleteHandling: "Disabled",
+            /*businessBeginsHour: 0,
+            businessEndsHour: 24,
+            businessWeekends: true,
+            durationBarVisible: false,
+            showNonBusiness: true,*/
+            //timeRangeSelectedHandling: "Disabled",
+            // eventDeleteHandling: "Disabled",
+           // onEventClick: this.onEventClick
+        };
+    }
 
-  get calendar() {
-    return this.calendarRef.current.control;
-  }
+    get calendar() {
+        return this.calendarRef.current.control;
+    }
 
-  componentDidMount() {
+    get datePicker() {
+        return this.datePickerRef.current.control;
+    }
+    componentDidMount() {
 
-    const events = [
-      {
-        id: 1,
-        text: "Event 1",
-        start: "2023-03-07T10:30:00",
-        end: "2023-03-07T13:00:00"
-      },
-      {
-        id: 2,
-        text: "Event 2",
-        start: "2023-03-08T09:30:00",
-        end: "2023-03-08T11:30:00",
-        backColor: "#6aa84f"
-      },
-      {
-        id: 3,
-        text: "Event 3",
-        start: "2023-03-08T12:00:00",
-        end: "2023-03-08T15:00:00",
-        backColor: "#f1c232"
-      },
-      {
-        id: 4,
-        text: "Event 4",
-        start: "2023-03-06T11:30:00",
-        end: "2023-03-06T14:30:00",
-        backColor: "#cc4125"
-      },
-    ];
+        const events = [
+            {
+                id: 1,
+                text: "Event 1",
+                start: "2023-03-07T10:30:00",
+                end: "2023-03-07T13:00:00"
+            },
+            {
+                id: 2,
+                text: "Event 2",
+                start: "2023-03-08T09:30:00",
+                end: "2023-03-08T11:30:00",
+                backColor: "#6aa84f"
+            },
+            {
+                id: 3,
+                text: "Event 3",
+                start: "2023-03-08T12:00:00",
+                end: "2023-03-08T15:00:00",
+                backColor: "#f1c232"
+            },
+            {
+                id: 4,
+                text: "Event 4",
+                start: "2023-03-06T11:30:00",
+                end: "2023-03-06T14:30:00",
+                backColor: "#cc4125"
+            },
+            {
+                id: 5,
+                text: "Event 5",
+                start: "2023-03-12T11:30:00",
+                end: "2023-03-12T14:30:00",
+                backColor: "#cc4125"
+            },
+        ];
 
-    const startDate = "2023-03-07";
+        const startDate = "2023-03-07";
 
-    this.calendar.update({startDate, events});
+        this.calendar.update({ startDate, events });
+        this.datePicker.update({ events: events });
 
-  }
+    }
 
-  render() {
-    return (
-      <div style={styles.wrap}>
-        <div style={styles.left}>
-          <DayPilotNavigator
-            selectMode={"week"}
-            showMonths={3}
-            skipMonths={3}
-            startDate={"2023-03-07"}
-            selectionDay={"2023-03-07"}
-            onTimeRangeSelected={ args => {
-              this.calendar.update({
-                startDate: args.day
-              });
-            }}
-          />
-        </div>
-        <div style={styles.main}>
-          <DayPilotCalendar
-            {...this.state}
-            ref={this.calendarRef}
-          />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='flex_Hundred'>
+                <div style={styles.left}>
+                    <DayPilotNavigator
+                        selectMode={"week"}
+                        showMonths={3}
+                        skipMonths={3}
+                        startDate={"2023-03-07"}
+                        selectionDay={"2023-03-07"}
+                        onTimeRangeSelected={args => {
+                            this.calendar.update({
+                                startDate: args.day
+                            });
+                        }}
+                        ref= {this.datePickerRef}
+                    />
+                </div>
+                <div style={styles.main}>
+                    <DayPilotCalendar
+                        {...this.state}
+                        cellsMarkBusiness= {false}
+                        businessWeekends= {true}
+                        headerDateFormat= {"dddd"}
+                        viewType= {"Week"}            
+                        allowEventOverlap= {false}
+                        durationBarVisible= {false}
+                        timeRangeSelectedHandling= {"Disabled"}
+                        ref={this.calendarRef}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
