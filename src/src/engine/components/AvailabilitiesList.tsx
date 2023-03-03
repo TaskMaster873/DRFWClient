643d91970592aa9a44442c4169708f97a5dde550
DJLawton <70708792+DJLawton@users.noreply.github.com>
2023-03-03 14:17:44 -0500

    Update AvailabilitiesList.tsx

--- src/src/engine/components/AvailabilitiesList.tsx
+++ src/src/engine/components/AvailabilitiesList.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {Container, Table} from "react-bootstrap";
+import {NotificationManager} from "../api/NotificationManager";
+import {errors} from "../messages/FormMessages";
 import {Employee} from "../types/Employee";
 import {unavailabilitiesTableHeads} from "../types/EmployeeAvailabilities";
 
@@ -11,18 +13,21 @@ type Props = {
 
 export class AvailabilitiesList extends React.Component<Props> {
 
-    private renderEmployeeNameFromId(employeeId: string, index: number) {
+    /**
+     * Creates the html elements of the employees name for the html table data
+     * @param employeeId The employee's id
+     * @param index the current table index
+     * @returns {JSX.Element} the html elements for the employees name
+     */
+    private employeeNameFromId(employeeId: string, index: number): JSX.Element {
         const currentEmployee = this.props.employees.find((e) => e.id == employeeId);
         if (!currentEmployee) {
+            NotificationManager.error(errors.ERROR_GENERIC_MESSAGE, errors.GET_EMPLOYEES)
             return (
-                <>
-                    <td key={"firstName " + index}>
-                        N/a
-                    </td>
-                    <td key={"lastName " + index}>
-                        N/a
-                    </td>
-                </>
+                <div>
+                    <td key={"firstName " + index}/>
+                    <td key={"lastName " + index}/>
+                </div>
             );
         }
         return (
@@ -42,12 +47,12 @@ export class AvailabilitiesList extends React.Component<Props> {
      * @private
      * @memberof AvailabilitiesList
      */
-    private unavailabilitiesList() {
+    private unavailabilitiesList(): JSX.Element[] {
         if (this.props.unavailabilities.length !== 0) {
             return this.props.unavailabilities.map((unavailability: any, index: number) => (
                 <tr key={"secondCol" + index}>
                     <td key={"no" + index}>{index + 1}</td>
-                    {this.renderEmployeeNameFromId(unavailability.employeeId, index)}
+                    {this.employeeNameFromId(unavailability.employeeId, index)}
                     <td key={"start " + index}>
                         {unavailability.start}
                     </td>
